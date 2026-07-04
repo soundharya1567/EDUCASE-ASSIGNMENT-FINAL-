a0c86707d3d17a9d0f8b8e1b6d7ba5326902c9d1
soundharya1567 <152592010+soundharya1567@users.noreply.github.com>
2026-07-05 02:25:27 +0530

    Welcome.jsx

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,20 +1,15 @@
 import { useNavigate } from "react-router-dom";
-
 function Welcome() {
   const navigate = useNavigate();
-
   return (
     <div className="container">
-      {/* Added 'welcome-screen' class here */}
       <div className="mobile welcome-screen">
         <div className="bottom">
           <h1>Welcome to PopX</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
           <button className="createBtn" onClick={() => navigate("/signup")}>
             Create Account
           </button>
-
           <button className="loginBtn" onClick={() => navigate("/login")}>
             Already Registered? Login
           </button>
@@ -23,5 +18,4 @@ function Welcome() {
     </div>
   );
 }
-
 export default Welcome;
