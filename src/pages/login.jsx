93b26ab6d04e206141a11e5194fe14c5f80683ff
soundharya1567 <152592010+soundharya1567@users.noreply.github.com>
2026-07-05 01:43:38 +0530

    login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,26 +1,19 @@
 import { useNavigate } from "react-router-dom";
-
 function Login() {
   const navigate = useNavigate();
-
   const handleLogin = (e) => {
     e.preventDefault();
-
     // always go to account page
     navigate("/account");
   };
-
   return (
     <div className="container">
       <div className="mobile">
         <form className="form" onSubmit={handleLogin}>
           <h2>Sign in to your PopX account</h2>
-
           {/* DISABLED INPUTS */}
           <input type="text" placeholder="Email Address" disabled />
-
           <input type="password" placeholder="Password" disabled />
-
           <button type="submit" className="submitBtn">
             Login
           </button>
